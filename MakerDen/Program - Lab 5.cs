88709c7046650fc2a66dfc395c81cb1fb328da61
whozDougie <dougleung@live.com.au>
2015-04-11 08:38:13 +1000

    Notes for output for NeoPixelJewel

---
+++ MakerDen/Program - Lab 5.cs
@@ -0,0 +1,35 @@
+﻿using Coatsy.Netduino.NeoPixel;
+using Coatsy.Netduino.NeoPixel.Grid;
+using Glovebox.MicroFramework.Sensors;
+using Glovebox.Netduino.Actuators;
+using Glovebox.Netduino.Sensors;
+using Microsoft.SPOT;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.Threading;
+
+namespace MakerDen {
+    public class Program : MakerBaseIoT {
+
+        public static void Main() {
+            // main code marker
+
+            //Replace the "emul" which is the name of the device with a unique 3 to 5 character name
+            //use your initials or something similar.  This code will be visible on the IoT Dashboard
+            //StartNetworkServices("WhozD", true);
+            StartNetworkServices("WhozD", true, "5C-86-4A-01-15-02");
+
+            using (SensorTemp temp = new SensorTemp(Pins.GPIO_PIN_D8, 10000, "temp01"))
+            using (SensorLight light = new SensorLight(AnalogChannels.ANALOG_PIN_A0, 1000, "light01"))
+            using (rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01"))
+            {
+                temp.OnBeforeMeasurement += OnBeforeMeasure;
+                temp.OnAfterMeasurement += OnMeasureCompleted;
+                light.OnBeforeMeasurement += OnBeforeMeasure;
+                light.OnAfterMeasurement += OnMeasureCompleted;
+                Thread.Sleep(Timeout.Infinite);
+            }
+
+
+        }
+    }
+}
